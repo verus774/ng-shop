a278effa048dc56e1be1a3f1a6a817c3a22dfd3a
Siarhei Ladutska <siarhei_ladutska@epam.com>
2019-05-22 17:25:41 +0300

    add ProductsService

---
+++ src/app/products/products.service.ts
@@ -0,0 +1,48 @@
+import {Injectable} from '@angular/core';
+
+import {Observable, of} from 'rxjs';
+
+import {ProductModel} from './models/product.model';
+import {CategoryModel} from './models/category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  products = [
+    new ProductModel(
+      1,
+      'Product 1',
+      'some product description',
+      CategoryModel.ebooks,
+      100,
+      false,
+      ['tag1', 'tag2', 'tag3']
+    ),
+    new ProductModel(
+      2,
+      'Product 2',
+      'some product description',
+      CategoryModel.laptops,
+      200,
+      true,
+      ['tag1', 'tag2', 'tag3']
+    ),
+    new ProductModel(
+      3,
+      'Product 3',
+      'some product description',
+      CategoryModel.mobile,
+      500,
+      false,
+      ['tag1', 'tag2', 'tag3']
+    )
+  ];
+
+  constructor() {
+  }
+
+  getProducts(): Observable<ProductModel[]> {
+    return of(this.products);
+  }
+}
