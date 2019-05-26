0e0dc72929ee92fa8380135b3dd70b229f45fe02
Sergey Ladutko <verus774@mail.ru>
2019-05-26 22:45:21 +0300

    add total sum

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,17 +1,17 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
-import {CartComponent} from './components/cart/cart.component';
+import {CartListComponent} from './components/cart-list/cart-list.component';
 
 @NgModule({
   declarations: [
-    CartComponent,
+    CartListComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    CartComponent,
+    CartListComponent,
   ]
 })
 export class CartModule {
